a35f8a1dfd8f2b19b7a5dd011385a3a07b505fca
Emmanuel <emmanuelcalad@gmail.com>
2024-02-12 13:44:19 -0500

    Add ejercicio3

---
+++ Ejercicios/ejercicio3.py
@@ -0,0 +1,9 @@
+import random
+#Crear un programa que genere una lista de números aleatorios y los imprima en pantalla.
+n=int(input("Ingrese el tamaño de la lista"))
+lista_numeros=[]
+
+for i in range(0, n):
+    lista_numeros.append(random)
+
+print(lista_numeros)
--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import random
 
+n = int(input("Ingrese el tamaño de la lista"))
+
+lista_numeros = []
+
+for i in range(0, n):
+
+    lista_numeros.append(random.randint(1 , 100))
+
+print(lista_numeros)
