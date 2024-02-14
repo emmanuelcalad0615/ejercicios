e34c9420942e98a3b5551de89a3081d3fda78c75
Administrator <castillosowin1@gmail.com>
2024-02-14 12:45:19 -0300

    .

--- Ejercicios/ejercicio 5.py
+++ Ejercicios/ejercicio 5.py
@@ -1,7 +1,7 @@
 #Escribir una función que calcule el factorial de un número dado
 
 def factorial(x):
-    if x == 0 or n == 1:
+    if x == 0 or x == 1:
         resultado=1
     elif x > 1:
         resultado=x*factorial(x-1)
