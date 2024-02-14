1046aaf3b2f3935cf3dee7517458d6438c4af1bc
Administrator <castillosowin1@gmail.com>
2024-02-14 12:13:22 -0300

    .

---
+++ Ejercicios/ejercicio 4.py
@@ -0,0 +1,8 @@
+
+#Escribir un programa que determine si un número dado es par o impar.
+
+def es_par_impar(numero):
+    if numero % 2 == 0:
+        return "El Numero es par"
+    else:
+        return "El numero es impar"
