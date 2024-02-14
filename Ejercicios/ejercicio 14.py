9e5fa1f7fa087246ce8a0ca434ea55128b8c3dea
Administrator <castillosowin1@gmail.com>
2024-02-14 13:02:29 -0300

    .

---
+++ Ejercicios/ejercicio 14.py
@@ -0,0 +1,18 @@
+#Escribir una función que calcule la media aritmética de una lista de números
+
+def calcular_media(lista):
+    if len(lista) == 0
+        return 0
+    suma = sum(lista)
+    media = suma / len(lista)
+    return media
+
+#prueba de uso
+
+"""""
+
+numeros = [2,3,4,6,7,10]
+media = calcular_media(numeros)
+print("La media de la lista es: ", media)
+
+"""""
