28a37dc1505ef8888a4d7c09e825fbab8c4add77
Administrator <castillosowin1@gmail.com>
2024-02-14 12:55:42 -0300

    .

---
+++ Ejercicios/ejercicio 12.py
@@ -0,0 +1,8 @@
+#Crear un programa que imprima los números del 1 al 10 utilizando un ciclo for.
+
+numeros = []
+
+for x in range(1,11):
+    numeros.append(x)
+
+print("numeros del 1 al 10: ", numeros)
